7e725ddea9b0994deecb2e63ae8828a45f51c3ef
Dacanizares <dacanizares@outlook.com>
2014-10-01 15:47:49 -0500

    Discussion problem A 17-09-2014

--- Training/UCO-Training-17-09-2014/Solutions/source/Hardwood.cpp
+++ Training/UCO-Training-17-09-2014/Solutions/source/Hardwood.cpp
@@ -2,25 +2,34 @@
 
 using namespace std;
 
+// Casos de prueba
 int c;
+// Mapa de las especiea
 map<string,double>trees;
-
 
 int main()
 {
     while(cin>>c){
-        string l;getline(cin,l);
+        string l; getline(cin,l);
         for(int t = 0; t < c; ++t){
             double pob = 0;
+            // Lee las especies del caso de prueba
             while(getline(cin,l)){
+                // Termina con linea vacia
                 if(l == "")break;
+                // Cuenta la especie
                 pob++;
+                // Si no esta en el mapa lo agrega
                 if(!trees.count(l))
                     trees[l]=0;
+                // Anade +1 a la cuenta de la especie
                 trees[l]++;
             }
+            // Recorremos el mapa
             for(map<string,double>::iterator it = trees.begin(); it != trees.end();++it){
+                // Imprimimos la especia
                 cout<<it->first<<" ";
+                // Imprimimos el % con respecto a la poblacion
                 cout<<fixed<<setprecision(4)<<(it->second/pob*100)<<"\n";
             }
             trees.clear();
